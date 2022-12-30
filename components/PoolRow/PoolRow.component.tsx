7eda63c7558aeebfc033a6193825d0e69e0a1572
Kacper Michalik <kacpermichalik3@gmail.com>
2022-12-30 15:17:10 +0100

    fix vercel

--- components/PoolRow/PoolRow.component.tsx
+++ components/PoolRow/PoolRow.component.tsx
@@ -1,10 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
 import { HeadingH3, HeadingH5 } from "../Typography/Typography.component";
 import { StyledArrowButton, StyledLine, StyledWrapper } from "./PoolRow.style";
 import { IPoolRow } from "./PoolRow.type";
 import ArrowIcon from "../../assets/arrow.svg";
-import Image from "next/image";
 import { formatPriceRatio } from "../../utils/helpers";
 
 const PoolRow: FC<IPoolRow> = ({
